1c07c2c31010e7b7c5d3c68981d57497fb8e80be
Samantha Hering <samanthahering@yahoo.com>
2020-09-16 18:54:51 -0400

    setup

---
+++ client/store/index.js
@@ -0,0 +1,16 @@
+import { createStore, applyMiddleware } from 'redux';
+import users from './users';
+import thunkMiddleware from 'redux-thunk';
+import { createLogger } from 'redux-logger';
+
+const store = createStore(
+    users,
+    applyMiddleware(
+      thunkMiddleware,
+      createLogger()
+    )
+  );
+  
+  export default store;
+
+
---
+++ client/store/users.js
@@ -0,0 +1,5 @@
+//dummy reducer for set up
+function dummyReducer (state = {}, action) {
+    return state;
+  }
+  export default dummyReducer
